cf99ba8f20fd8b63365fa8f4d414fc3b786000a2
Olawamide Moyinoluwa <134518428+MOYIN-21@users.noreply.github.com>
2023-12-10 08:12:26 +0100

    add these (#1)

--- src/pages/about-Jesus/AboutJesus.jsx
+++ src/pages/about-Jesus/AboutJesus.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './AboutJesus.css'
+import crownImage2 from '../../assets/image/crownImage2.svg'
+// import './assets/image/way-maker.png'
 
 export default function AboutJesus() {
   return (
     <div>
-      About Jesus
+      <div className="Image-Frame"><img src={crownImage2} alt="Ade-Egun-Ati-To-Fine"/></div>
+     <div className='h1-div'><h1>This Jesus is...</h1></div>
+      
+      <div className="way-maker"><img src="{way-maker}" alt="" /></div>
+      <div className='p-tag'><p></p></div>
+
+
+
+
+
+
+
     </div>
+   
   )
 }
